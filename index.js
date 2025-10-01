9fce89b61741f5f3e064cfb752365e9d22a0faf2
William <william.jiang1101@gmail.com>
2025-10-01 15:05:20 -0400

    fixed

--- index.js
+++ index.js
@@ -3,24 +3,40 @@ const fiveToOneHundred = () => {
     for (let i = 5; i <= 100; i++) {
         console.log(i);
     }
+    console.log('==========');
 }
 
 fiveToOneHundred();
 
 const multiplesOfThree = () => {
     for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0) {
+            console.log(i);
+        }
+    }
+    console.log('==========');
+}
+
+multiplesOfThree();
+
+const multiplesOfThreeOrFive = () => {
+    for (let i = 1; i <= 100; i++) {
         if (i % 3 === 0 || i % 5 === 0) {
             console.log(i);
         }
     }
+    console.log('==========');
 }
 
-multiplesOfThree();
+multiplesOfThreeOrFive();
 
 const untilNum = (num) => {
     for (let i = 1; i <= num; i++) {
         console.log(i);
     }
+    console.log('==========');
 }
 
 untilNum(42);
+
+
