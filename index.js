fa02d28bfabc20be1ac9202f64f0baefaa733889
William <william.jiang1101@gmail.com>
2025-10-02 15:47:25 -0400

    function 12

--- index.js
+++ index.js
@@ -100,3 +100,18 @@ console.log(largest(4, 6, 8));
 console.log(largest(30, 22, 17));
 console.log(largest(41, 108, 86));
 console.log('========== P10');
+
+const isLeapYear = (year) => {
+    if (year % 4 === 0 && year % 400 != 0 && year % 100 === 0) {
+        return false;
+    } else if (year % 400 === 0) {
+        return true;
+    } else if (year % 4 === 0 && year % 100 != 0) {
+        return true;
+    } else
+        return false;
+}
+
+console.log(isLeapYear(2024));
+console.log(isLeapYear(1900));
+console.log(isLeapYear(2000));
