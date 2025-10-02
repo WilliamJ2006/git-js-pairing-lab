965181cecfce4bbe5a4c571ebd65bd187236e3ff
William <william.jiang1101@gmail.com>
2025-10-02 15:54:38 -0400

    function 14

--- index.js
+++ index.js
@@ -12,3 +12,18 @@ const isLeapYear = (year) => {
 console.log(isLeapYear(2024));
 console.log(isLeapYear(1900));
 console.log(isLeapYear(2000));
+console.log('========== P12');
+
+const absoluteNineteen = (num) => {
+    let finalNum = 0;
+    finalNum = Math.abs(num - 19);
+    if (finalNum > 19) {
+        finalNum = finalNum * 3;
+    }
+    return finalNum;
+}
+
+console.log(absoluteNineteen(54));
+console.log(absoluteNineteen(10));
+console.log(absoluteNineteen(5));
+console.log('========== P14');
